513ff5a1bfed5ac1623a4a8cda6753928374e411
cseazeem <azeema224143@gmail.com>
2024-08-22 09:20:28 +0530

    exception-handling

---
+++ src/main/java/com/cseazeem/supplier/exception/SupplierNotFoundException.java
@@ -0,0 +1,7 @@
+package com.cseazeem.supplier.exception;
+
+public class SupplierNotFoundException extends RuntimeException {
+    public SupplierNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/cseazeem/supplier/service/SupplierService.java
+++ src/main/java/com/cseazeem/supplier/service/SupplierService.java
@@ -2,6 +2,7 @@ package com.cseazeem.supplier.service;
 
 import com.cseazeem.supplier.database.SupplierDaoImpl;
 import com.cseazeem.supplier.entity.Supplier;
+import com.cseazeem.supplier.exception.SupplierNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,7 +16,14 @@ public class SupplierService {
         this.supplierDaoImpl = supplierDaoImpl;
     }
 
-    public List<Supplier> searchSuppliers(String location, Supplier.NatureOfBusiness natureOfBusiness, Supplier.ManufacturingProcesses manufacturingProcess, int pageNo, int pageSize) {
-        return supplierDaoImpl.searchSuppliers(location, natureOfBusiness, manufacturingProcess, pageNo, pageSize);
+    public List<Supplier> searchSuppliers(String location, Supplier.NatureOfBusiness natureOfBusiness, Supplier.ManufacturingProcesses manufacturingProcess, int limit, int offset) {
+        List<Supplier> suppliers = supplierDaoImpl.searchSuppliers(location, natureOfBusiness, manufacturingProcess, limit, offset);
+
+        if (suppliers.isEmpty()) {
+            throw new SupplierNotFoundException("No suppliers found with the given criteria");
+        }
+
+        return suppliers;
     }
+
 }
